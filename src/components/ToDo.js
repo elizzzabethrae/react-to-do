2e131307870b50a929b21329d7ffa89ff48e69d2
elizzzabethrae <elizabethwarners@gmail.com>
2018-08-02 21:59:18 -0400

    Checkpoing props and state

---
+++ src/components/ToDo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class ToDo extends Component {
+  render() {
+    return (
+      <li>
+        <input type="checkbox" checked={ this.props.isCompleted } />
+        <span> { this.props.description } </span>
+      </li>
+    );
+  }
+}
+
+export default ToDo;
