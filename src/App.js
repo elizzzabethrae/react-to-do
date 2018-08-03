b4811dbda8d3b9279f46c792aac56b3dd084d201
elizzzabethrae <elizabethwarners@gmail.com>
2018-08-02 22:15:18 -0400

    i am very lost and dont know what I'm doing

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react';
 import './App.css';
+import ToDo from './components/ToDo.js';
 
 class App extends Component {
+  constructor(props) {
+  super(props);
+  this.state = {
+       todos: [
+         { description: 'Walk the cat', isCompleted: true },
+         { description: 'Throw the dishes away', isCompleted: false },
+         { description: 'Buy new dishes', isCompleted: false }
+       ]
+     };
+}
+
   render() {
     return (
       <div className="App">
-      
-      </div>
-    );
-  }
-}
+        <ul>
+          { this.state.todos.map( (todo, index) =>
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } />
+           )}
+         </ul>
+       </div>
+     );
+   }
+ }
+
 
 export default App;
